a8cbb462c2581e84ef1ceecec89e71a32e2b0bf2
Vojta Oupicky <voj.oup@gmail.com>
2018-02-14 20:57:21 +0100

    More tests

--- main.test.js
+++ main.test.js
@@ -5,3 +5,10 @@ test('adds 1 + 2 to equal 3', addTest);
 function addTest() {
     expect(add(1, 2)).toBe(3);
 }
+
+test('adds 0 + 0 to equal 0', addTest);
+
+function addTest() {
+    expect(add(0, 0)).toBe(0);
+}
+
